dc6aad57bae6ba984c693fab8cb561de30142a78
Jor Bashllari <altnate2000@gmail.com>
2018-11-06 12:49:04 +0100

    fix putnbr_base

--- lib/my/my_showstr.c
+++ lib/my/my_showstr.c
@@ -9,35 +9,28 @@
 
 void my_showstr(char const *str)
 {
-    char *base = "0123456789abcdef";
-    char test[2];
-    test[1] = 0;
-
-    for (int i = 0; str[i] != '\0'; i++) {
-        test[0] = str[i];
-        if (my_str_isprintable(test))
+    for (unsigned int i = 0; str[i]; i++) {
+        if (my_isprintable(str[i]))
             my_putchar(str[i]);
         else {
             my_putchar('\\');
             if (str[i] < 16)
                 my_putchar('0');
-            my_put_nbr_base(str[i], base);
+            my_put_nbr_base(str[i], "0123456789abcdef");
         }
     }
 }
-
+/*
 int my_put_nbr_base(int n, char const *base)
 {
-    int i = 0;
+    int i = my_strlen(base);
     int leftovers = 0;
 
-    while (base[i] != '\0')
-        i++;
     if (i <= 1)
         return n;
     leftovers %= i;
     if (leftovers < 0)
-        leftovers = -leftovers;
+        leftovers *= -1;
     if (n / i == 0) {
         if (n < 0)
             my_putchar('-');
@@ -47,4 +40,26 @@ int my_put_nbr_base(int n, char const *base)
     my_put_nbr_base(n / i, base);
     my_putchar(base[leftovers]);
     return n;
+}*/
+
+int my_put_nbr_base(int nb, char const *base)
+{
+    int b = my_strlen(base);
+
+    if (nb < INT_MIN + b + 1 || nb > INT_MAX - b - 1) {
+        write(2, "put_nbr: out of bounds\n", 26);
+        return 0;
+    }
+    if (nb < 0) {
+        my_putchar('-');
+        my_put_nbr_base(-nb, base);
+    }
+    if (nb > b - 1) {
+        my_put_nbr_base(nb / b, base);
+        my_put_nbr_base(nb % b, base);
+        return 0;
+    }
+    if (nb >= 0 && nb < b)
+        my_putchar(base[nb]);
+    return 0;
 }
