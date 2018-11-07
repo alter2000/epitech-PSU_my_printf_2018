8a8cdf9ac6b320273bc7f6a855aaecbdebb50bd1
Jor Bashllari <altnate2000@gmail.com>
2018-11-07 17:23:46 +0100

    finish flags? no.

--- lib/my/my_printf/my_printf.c
+++ lib/my/my_printf/my_printf.c
@@ -6,6 +6,7 @@
 */
 
 #include "my.h"
+#include "my_printf.h"
 
 int my_printf(char const *fmt, ...)
 {
@@ -26,5 +27,56 @@ int pstuff(char const *fmt, char const **args, va_list ap)
             my_putchar(*(fmt++));
         else
             fmt++;
-    
+
 }
+
+int eval_flags(char const *fmt)
+{
+    int i = 0;
+    unsigned int flags = 0U;
+
+    do {
+        switch (*fmt) {
+            case '#':
+                flags |= FLAG_HASH;
+                fmt++;
+                i = 1;
+                break;
+            case '0':
+                flags |= FLAG_ZERO;
+                fmt++;
+                i = 1;
+                break;
+            case '-':
+                flags |= FLAG_LEFT;
+                fmt++;
+                i = 1;
+                break;
+            case '+':
+                flags |= FLAG_SIGN;
+                fmt++;
+                i = 1;
+                break;
+            case ' ':
+                flags |= FLAG_SPACE;
+                fmt++;
+                i = 1;
+                break;
+            default:
+                i = 0;
+                break;
+        }
+    } while (i);
+    return flags;
+}
+
+int eval_len(char const *fmt)
+{
+    int i = 0;
+    unsigned int len = 0U;
+
+    do {
+    } while (i);
+    return len;
+}
+
--- lib/my/my_printf/my_printf.h
+++ lib/my/my_printf/my_printf.h
@@ -12,18 +12,22 @@
 
 /* like UNIX permissions weeeee */
 
-#   define LEN_CHAR
-#   define LEN_SHORT
-#   define LEN_LONG
-#   define LEN_LLONG
+#   define LEN_CHAR    (1U << 0U)
+#   define LEN_SHORT   (1U << 1U)
+#   define LEN_LONG    (1U << 2U)
+#   define LEN_LLONG   (1U << 3U)
+#   define LEN_IMAX    (1U << 4U)
+#   define LEN_SIZE    (1U << 5U)
+#   define LEN_PTRDIFF (1U << 6U)
 
-#   define FLAG_HASH
-#   define FLAG_ZERO
-#   define FLAG_LEFT
-#   define FLAG_SPACE
-#   define FLAG_SIGN
-#   define FLAG_PRECISION
+#   define FLAG_HASH      (1U << 7U)
+#   define FLAG_ZERO      (1U << 8U)
+#   define FLAG_LEFT      (1U << 9U)
+#   define FLAG_SPACE     (1U << 10U)
+#   define FLAG_SIGN      (1U << 11U)
+#   define FLAG_PRECISION (1U << 12U)
 
+// my_printf.c
 int my_printf(char const *fmt, ...);
 
 int pstuff(char const *fmt, char const **args, va_list ap);
