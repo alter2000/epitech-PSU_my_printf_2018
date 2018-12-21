6126fd40212b608f4f08908bc31735d70a0ca585
Jor Bashllari <altnate2000@gmail.com>
2018-12-21 18:19:45 +0100

    split option switch

--- lib/my/my_printf/my_printf.c
+++ lib/my/my_printf/my_printf.c
@@ -42,25 +42,69 @@ int pstuff(char const *fmt, va_list ap)
     return ch;
 }
 
+unsigned int more_extended_put(char const **fmt, va_list ap)
+{
+    unsigned int ch = 0;
+
+    switch (**fmt) {
+        case 'S': ++(*fmt);
+                ch += p_showstr(va_arg(ap, char *));
+                break;
+        case 'p': ++(*fmt);
+                ch += p_showptr(va_arg(ap, size_t));
+                break;
+        case '%': ++(*fmt);
+                ch += my_putchar('%');
+                break;
+        default: ch += my_putchar('%');
+                ch += my_putchar(*(++(*fmt)));
+                break;
+    }
+    return ch;
+}
+
+unsigned int extended_put(char const **fmt, unsigned int flags, \
+        unsigned int width, unsigned int prec, va_list ap)
+{
+    unsigned int ch = 0;
+
+    switch (**fmt) {
+        case 'o': ++(*fmt);
+                ch += p_oct(flags, width, prec, ap);
+                break;
+        case 'b': ++(*fmt);
+                ch += p_bin(flags, width, prec, ap);
+                break;
+        case 'c': ++(*fmt);
+                ch += my_putchar((char) va_arg(ap, int));
+                break;
+        case 's': ++(*fmt);
+                ch += p_putstr(va_arg(ap, char *), flags);
+                break;
+        default: ch += more_extended_put(fmt, ap);
+    }
+    return ch;
+}
+
 unsigned int put(char const **fmt, unsigned int flags, \
         unsigned int width, unsigned int prec, va_list ap)
 {
     unsigned int ch = 0;
 
     switch (**fmt) {
-        case 'd':
-        case 'i': ++(*fmt); ch += p_dec(flags, width, prec, ap); break;
-        case 'u': ++(*fmt); ch += p_udec(flags, width, prec, ap); break;
-        case 'x': ++(*fmt); ch += p_hex(flags, width, prec, ap); break;
-        case 'X': ++(*fmt); ch += p_chex(flags, width, prec, ap); break;
-        case 'o': ++(*fmt); ch += p_oct(flags, width, prec, ap); break;
-        case 'b': ++(*fmt); ch += p_bin(flags, width, prec, ap); break;
-        case 'c': ++(*fmt); ch += my_putchar((char) va_arg(ap, int)); break;
-        case 's': ++(*fmt); ch += p_putstr(va_arg(ap, char *), flags); break;
-        case 'S': ++(*fmt); ch += p_showstr(va_arg(ap, char *)); break;
-        case 'p': ++(*fmt); ch += p_showptr(va_arg(ap, size_t)); break;
-        case '%': ++(*fmt); ch += my_putchar('%'); break;
-        default: break;
+        case 'd': case 'i': ++(*fmt);
+                ch += p_dec(flags, width, prec, ap);
+                break;
+        case 'u': ++(*fmt);
+                ch += p_udec(flags, width, prec, ap);
+                break;
+        case 'x': ++(*fmt);
+                ch += p_hex(flags, width, prec, ap);
+                break;
+        case 'X': ++(*fmt);
+                ch += p_chex(flags, width, prec, ap);
+                break;
+        default: ch += extended_put(fmt, flags, width, prec, ap);
     }
     return ch;
 }
